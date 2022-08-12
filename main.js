ece8bddaf92399240278612928857f4ab194ce29
Connor Broad <connor.broad@gmail.com>
2022-08-13 01:22:41 +1200

    input listeners

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { Player } from './player.js'
-import { InputHandler } from './input.js'
+import { InputHandler } from './input.js' 
 
 class Game {
     constructor (width, height){
@@ -12,32 +12,29 @@ class Game {
 
     //triggered every frame
     update(){
-        this.player.update();
+        this.player.update(this.input.keysPressed);
     }
 
     draw(context){
         this.player.draw(context);
     }
-}
+} 
 
-function setUpAnimation(game, ctx, canvas){
-    function animate(){
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        game.update();
-        game.draw(ctx);
-        requestAnimationFrame(animate);
-    }
-    animate();
-}
+function animate(timestamp){ 
+    window.ctx.clearRect(0, 0, canvas.width, canvas.height);
+    window.game.update();
+    window.game.draw(window.ctx);
+    requestAnimationFrame(animate);
+} 
 
+// starting the game here ensures all images are loaded
 window.addEventListener('load', function() {
-    const canvas = document.getElementById("gameCanvas");
-    canvas.width = 500;
-    canvas.height = 500; 
+    window.canvas = document.getElementById("gameCanvas");
+    window.canvas.width = 500;
+    window.canvas.height = 500; 
 
-    const game = new Game(canvas.width, canvas.height);
-    console.log(game);
-
-    const ctx = canvas.getContext('2d');
-    setUpAnimation(game, ctx, canvas);
+    window.game = new Game(canvas.width, canvas.height); 
+    window.ctx = canvas.getContext('2d');
+ 
+    window.requestAnimationFrame(animate);
 })
--- player.js
+++ player.js
@@ -3,18 +3,28 @@ export class Player {
         this.game = game;
 
         this.width = 100;
-        this.height = 100;
+        this.height = 91.3;
 
         this.x = 0;
-        this.y = this.game.height - this.height; 
+        this.y = this.game.height - this.height;
+
+        this.image = document.getElementById("player");
     }
 
-    update() {
-        this.x++;
+    update(inputKeys) { 
+        console.log(inputKeys);
+        if(inputKeys.includes('ArrowRight')) this.x++;
+        if(inputKeys.includes('ArrowLeft')) this.x--;
+        if(inputKeys.includes('ArrowUp')) this.y--;
+        if(inputKeys.includes('ArrowDown')) this.y++;
     }
     
     draw(context){
         context.fillStyle = 'red';
-        context.fillRect(this.x, this.y, this.width, this.height); 
+        context.fillRect(this.x, this.y, this.width, this.height);
+
+        // let cropX = 0;
+        // let cropY = 0;
+        // context.drawImage(this.image, cropX, cropY, this.width, this.height, this.x, this.y, this.width, this.height)
     }
 }
